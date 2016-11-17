9d775d147f955a6220498f850a034336d3789ad2
Razvan Moraru <razvan@inclub.ro>
2016-11-17 12:21:05 +0100

    chore: improve reportCoverage.sh tool

--- tools/reportCoverage.sh
+++ tools/reportCoverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #report coverage to codacy-coverage service
-cat ./coverage/lcov.info | ./node_modules/.bin/codacy-coverage -t 4c3cae3042484b739e1d7f786d5b136a
+./node_modules/.bin/codacy-coverage < ./coverage/lcov.info -t 4c3cae3042484b739e1d7f786d5b136a
 rm -rf ./coverage
 
